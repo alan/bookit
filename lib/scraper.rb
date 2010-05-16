3ecf9c6911b2627398aa265f5747b67684512ba2
alan <alankennedy100@gmail.com>
2010-05-16 19:43:13 +0100

    updated scraper to new page layout

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,16 +6,16 @@ module AutomateAT
     end
     
     def get_available_courts
-      data = @raw_data.xpath('//tr')
+      data = @raw_data.xpath('//tr')[1..16]
       dates = get_bookable_dates(@raw_data)
       
       availability = data.inject({}) do |result, row|
         clean_data = row.css('td')
-        time = clean_data.delete(clean_data.first).text
-      
+        time = row.css('th')[0].text
+                
         dates.each_with_index do |date, i|
           if clean_data[i].text =~ /Left/
-            result[date] = [] if(result[date].nil?)
+            result[date] = [] if result[date].nil?
             result[date] << time 
           end
         end
@@ -25,7 +25,7 @@ module AutomateAT
     end
   
     def get_bookable_dates(data)
-      row_of_dates = data.xpath("//tr[1]/td")
+      row_of_dates = data.xpath("//tr[1]/th")
       dates = row_of_dates.inject([]) do |result, cell|
         if cell.content != "Time"
           date = cell.children[0].text + ", " + cell.children[2]
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -27,13 +27,20 @@ describe AutomateAT::Scraper do
     it "should return a hash with the available times" do
       data = Nokogiri::HTML::Document.parse(File.open(File.dirname(__FILE__ )+ "/stubs/full_dom"))
       processor = AutomateAT::Scraper.new(data)
-      processor.get_available_courts.should == {"Monday, 7 Dec"=> ["8:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm"], 
-                                                 "Tuesday, 8 Dec"=>["12:00pm"], 
-                                                 "Saturday, 5 Dec"=>["8:00am"], 
-                                                 "Friday, 11 Dec"=>["7:00am", "8:00am", "1:00pm", "3:00pm", "8:00pm", "9:00pm"], 
-                                                 "Thursday, 10 Dec"=>["7:00am", "8:00am", "9:00am", "10:00am", "3:00pm", "10:00pm"], 
-                                                 "Wednesday, 9 Dec"=>["10:00pm"]
-                                                 }
+      
+      courts = processor.get_available_courts
+      courts["Monday, 7 Dec"].should include("8:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm")
+      courts["Monday, 7 Dec"].should have(5).items
+      courts["Tuesday, 8 Dec"].should include("12:00pm")
+      courts["Tuesday, 8 Dec"].should have(1).item
+      courts["Wednesday, 9 Dec"].should include("10:00pm")
+      courts["Wednesday, 9 Dec"].should have(1).item
+      courts["Thursday, 10 Dec"].should include("7:00am", "8:00am", "9:00am", "10:00am", "3:00pm", "10:00pm")
+      courts["Thursday, 10 Dec"].should have(6).items
+      courts["Friday, 11 Dec"].should include("7:00am", "8:00am", "1:00pm", "3:00pm", "8:00pm", "9:00pm")
+      courts["Friday, 11 Dec"].should have(6).items
+      courts["Saturday, 5 Dec"].should include("8:00am")
+      courts["Saturday, 5 Dec"].should have(1).item
     end
   end
 end
