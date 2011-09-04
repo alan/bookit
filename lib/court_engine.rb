61a851e3fb75b8eb3b210c72b31e56432e805c5c
Alan Kennedy <alankennedy100@gmail.com>
2011-09-04 21:29:41 +0100

    Delete whitespace

--- lib/court_engine.rb
+++ lib/court_engine.rb
@@ -1,27 +1,27 @@
 module AutomateAT
   class CourtEngine
-    
+
     attr_reader :time_expiration, :adapter
-    
+
     def initialize(time_expiration = 30 * 60)
-      @adapter = Redis.new(:host => '127.0.0.1', 
-                           :port => 6379, 
-                           :thread_safe => true, 
+      @adapter = Redis.new(:host => '127.0.0.1',
+                           :port => 6379,
+                           :thread_safe => true,
                            :db => Bookit::CONFIG["db"],
                            :logger => Bookit::logger)
       @time_expiration = time_expiration
     end
-    
+
     def delete_all
       adapter.flushdb
     end
-    
+
     def save_courts(courts)
       old_available = adapter.smembers("available")
       adapter.pipelined do
         old_available.each{|o| adapter.del(o)}
         adapter.del("available")
-    
+
         courts.each do |day, times|
           day_key = key("available", day)
           adapter.sadd("available", day_key)
@@ -29,23 +29,23 @@ module AutomateAT
         end
       end
     end
-    
+
     def courts_to_notify
       available_dates = adapter.smembers("available")
       available_dates.inject({}) do |result, availability|
-        
+
         # keys to be used
         wanted_key = matching_wanted_key_for(availability)
         date = human_date_from_availability_key(availability)
         to_notify_key = key("to_notify", date)
         notified_keys = adapter.smembers(key("notified", date))
-        
+
         # found => has the times that is available that I am interested
         adapter.sinterstore("found", availability, wanted_key)
         adapter.sdiffstore(to_notify_key, "found", *notified_keys)
-        
+
         data = adapter.smembers(to_notify_key)
-        
+
         if data.any?
           adapter.sadd("to_notify", to_notify_key)
           result[date] = data
@@ -53,7 +53,7 @@ module AutomateAT
         result
       end
     end
-    
+
     def user_notified
       to_notify = adapter.smembers("to_notify")
       to_notify.each do |key_name|
@@ -65,23 +65,23 @@ module AutomateAT
         adapter.expire(notified_key, time_expiration)
       end
     end
-    
+
     def key(*parts)
       parts.inject([]) {|result, part| result << part.gsub(' ', '-')}.join(':')
     end
-    
+
     def setup_wanted_times
       Bookit::CONFIG["wanted_times"].each do |day, times|
         times.split(', ').each{|time| adapter.sadd(key("wanted", day), time)}
       end
     end
-    
+
     private
-    
+
     def human_date_from_availability_key(key)
       key.gsub("available:", "").gsub("-", " ")
     end
-    
+
     def matching_wanted_key_for(date_key)
       day = %w(monday tuesday wednesday thursday friday saturday sunday).find{|d| date_key.downcase.include? d}
       key("wanted", day)
