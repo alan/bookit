207e0e5870a1b20cb5c37639a56c1d719de1407d
Alan Kennedy <alankennedy100@gmail.com>
2011-09-04 18:36:10 +0100

    Create log folder before starting the specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["APP_ENV"] = "test"
 require './config/boot'
 Bundler.require("test")
 
+
 Spec::Runner.configure do |config|
+  config.before(:all) { FileUtils.mkdir_p(File.join(ROOT, 'log')) }
   config.before(:each) { AutomateAT::Bookit.engine.delete_all }
 end
